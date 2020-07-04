6b21fd9a2d9bc1ee54820b268915d5b42c9cdfd7
AleksCh <ssss.utest@gmail.com>
2020-07-04 18:24:36 +0300

    ref

--- api/src/helpers/db.js
+++ api/src/helpers/db.js
@@ -1,23 +1,8 @@
 const mongoose = require("mongoose");
 const { db } = require("../configuration");
-const options = {
-    autoIndex: false, // Don't build indexes
-    reconnectTries: 30, // Retry up to 30 times
-    reconnectInterval: 500, // Reconnect every 500ms
-    poolSize: 10, // Maintain up to 10 socket connections
-    // If not connected, return errors immediately rather than waiting for reconnect
-    bufferMaxEntries: 0
-  }
 
 module.exports.connectDb = () => {
-  mongoose.connect(db, options).then(()=>{
-    console.log('MongoDB is connected')
-  }).catch(err=>{
-  console.log('MongoDB connection unsuccessful, retry after 5 seconds.')
-  setTimeout(connectWithRetry, 5000)
-  })
+  mongoose.connect(db, { useNewUrlParser: true });
+
   return mongoose.connection;
 };
-
-
-
--- api/src/index.js
+++ api/src/index.js
@@ -14,12 +14,18 @@ app.get("/test", (req, res) => {
   res.send("Our api server is working correctly");
 });
 
+app.get("/testapidata", (req, res) => {
+  res.json({
+    testapidata: true
+  });
+});
+
 app.get("/testwithcurrentuser", (req, res) => {
   axios.get(authApiUrl + "/currentUser").then(response => {
-     res.json({
-       testwithcurrentuser: true,
-       currentUserFromAuth: response.data
-       });
+    res.json({
+      testwithcurrentuser: true,
+      currentUserFromAuth: response.data
+    });
   });
 });
 
@@ -28,6 +34,7 @@ const startServer = () => {
     console.log(`Started api service on port ${port}`);
     console.log(`Our host is ${host}`);
     console.log(`Database url ${db}`);
+    console.log(`Auth api url ${authApiUrl}`);
 
     const silence = new Kitten({ name: "Silence" });
     silence.save(function(err, result) {
